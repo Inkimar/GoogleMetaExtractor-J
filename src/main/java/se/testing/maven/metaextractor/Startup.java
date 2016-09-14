739cda24b86cb733ac2b323faf2f4231cc91ecc7
Ingimar E <inkimar@gmail.com>
2016-09-14 15:04:33 +0200

    refactoring

--- src/main/java/se/testing/maven/metaextractor/Startup.java
+++ src/main/java/se/testing/maven/metaextractor/Startup.java
@@ -17,46 +17,71 @@ import se.testing.maven.metaextractor.util.MapWrapper;
  */
 public class Startup {
 
+    /**
+     * send in a path such as '/home/ingimar/Documents/gunvis-bilder/'
+     * @TODO , send in filter : if '-f .jpg' the run exif . 
+     * @param args 
+     */
     public static void main(String[] args) {
 
-        System.out.println("length  for args is " + args.length);
-        System.out.println("v0.0.3 ");
-        String fileDirectory = "";
+        String directory;
         if (args.length > 0) {
-            fileDirectory = args[0];
+            directory = args[0];
+            System.out.println("incoming path to file  is '" + directory + "'");
         } else {
-            fileDirectory = FilePropertiesHelper.getImagesFilePath();
-            if (fileDirectory == null || fileDirectory.isEmpty()) {
+            directory = FilePropertiesHelper.getImagesFilePath();
+            if (directory == null || directory.isEmpty()) {
                 throw new IllegalArgumentException("Not a valid path");
             }
+            System.out.println("path from 'config.properties' to file  is '" + directory + "'");
         }
 
-        final int numberOfFiles = getNumberOfFiles(fileDirectory);
-        System.out.println("path " + fileDirectory + " #" + numberOfFiles + " image(s)");
+        final int numberOfFiles = getNumberOfFiles(directory);
+        System.out.println("\t has  #" + numberOfFiles + " image(s)");
 
-        writeOutNameOfFiles(fileDirectory);
+        //writeFilenamesToConsole(directory);
 
-        MapWrapper container = populateMapWrapper(fileDirectory);
-        // List<String> listOfCatalogs = container.getListOfCatalogs();
+        MapWrapper container = populateMapWrapper(directory);
         List<String> listOfCatalogs = container.getSortedListOfCatalogs();
         printCatalogs(listOfCatalogs);
 
         final int sizeOfCatalogNumbers = container.getNumberOfCatalogs();
-        System.out.println("Size from container-method :  " + sizeOfCatalogNumbers);
+        System.out.println("Number of catalognumbers:  " + sizeOfCatalogNumbers +" number of files "+ numberOfFiles);
 
-        getMetaData(fileDirectory, true);
+        getMetaData(directory, true);
 
     }
 
+    /**
+     * example of returned mapper :
+     * 
+     * map[0] : NHRS-GULI000000004 => [labr, late, dors]
+     * map[1] : NHRS-GULI000000003 => [late, dors]
+     * map[2] : NHRS-GULI000000002 => [late, dors]
+     * map[3] : NHRS-GULI000000008 => [dors, late, vari, no-view-no]
+     * map[4] : NHRS-GULI000000007 => [pron, dors, late, dors, late, dors]
+     * map[5] : NHRS-GULI000000006 => [pron, late, dors, dors]
+     * map[6] : NHRS-GULI000000005 => [pron, late, late, dors, dors]
+     * 
+     * @param directory
+     * @return 
+     */
     private static MapWrapper populateMapWrapper(String directory) {
         File[] files = ListFilesUtil.getFiles(directory);
         List<String> fileNames = ListFilesUtil.getFileNames(files);
         return MapWrapper.getPopulatedMapWrapper(fileNames);
     }
 
+    /**
+     * Using the filter from the config.properties-file
+     * 
+     * @param directory
+     * @param withFilter 
+     */
     private static void getMetaData(String directory, boolean withFilter) {
         ExifExtract retriever = new ExifExtract();
         final String filter = FilePropertiesHelper.getImageFilter();
+        System.out.println("\n Filter that is used "+filter);
 
         List<JSONObject> listOfJSONobjects = null;
 
@@ -85,7 +110,7 @@ public class Startup {
 
     }
 
-    private static void writeOutNameOfFiles(String directoryLinuxMac) {
+    private static void writeFilenamesToConsole(String directoryLinuxMac) {
         File[] files = ListFilesUtil.getFiles(directoryLinuxMac);
         ListFilesUtil.printFileNamesToConsole(files);
     }
