81a2f9d9c067448814bc64a00622fb0d8b86aff8
Ingimar E <inkimar@gmail.com>
2016-09-14 15:34:10 +0200

    removing groovy-fil

--- change-file-name.groovy
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/groovy
-def name='World'; println "change file names"
-String oldFilename = "old.txt"  
-String newFilename = "new.txt"  
-  
-new File(oldFilename).renameTo(new File(newFilename))
-
-
